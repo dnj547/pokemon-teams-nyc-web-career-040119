1f85c3c7c4312edd9d8636cd6ac3185ab65d0828
github username <github email address>
2019-05-16 13:26:35 -0400

    fetched first name

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,15 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+fetch(TRAINERS_URL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object[0].name)
+    const main = document.querySelector("#main")
+    main.innerHTML += `
+    <div class="card" data-id="1"><p>${object[0].name}</p></div>`
+  })
+div1 = document.createElement("div")
